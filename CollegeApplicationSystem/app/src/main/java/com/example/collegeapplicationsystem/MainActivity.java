c2a3fc38d5f463ea8cbeca229b1fc4095b12c73e
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2019-11-18 18:58:51 -0500

    added guest login

--- CollegeApplicationSystem/app/src/main/java/com/example/collegeapplicationsystem/MainActivity.java
+++ CollegeApplicationSystem/app/src/main/java/com/example/collegeapplicationsystem/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.collegeapplicationsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -19,11 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN_REQUEST_CODE = 0;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         /* Example Query
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference docRef = db.collection("colleges").document("100654");
@@ -45,8 +54,43 @@ public class MainActivity extends AppCompatActivity {
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false)
+                        .setTheme(R.style.SignInTheme)
                         .build(),
                 RC_SIGN_IN_REQUEST_CODE);
+    }
+
+    public void anonSignInAndStartCollegeIntent(View view) {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startCollegeIntent();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    private void startCollegeIntent() {
+        Intent intent = new Intent(getApplicationContext(), MainCollegeSearchActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+       // updateUI(currentUser);
     }
 
     @Override
@@ -58,9 +102,11 @@ public class MainActivity extends AppCompatActivity {
 
             if (resultCode == RESULT_OK) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                // START THE REAL PURPOSE OF APP HERE, ELSE IT WILL RETURN TO LOGIN SCREEN
+                Toast.makeText(this, "Sign in successful!", Toast.LENGTH_SHORT).show();
 
+                startCollegeIntent();
             } else {
+                Toast.makeText(this, "Sign in failed!", Toast.LENGTH_SHORT).show();
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
                 // response.getError().getErrorCode() and handle the error.
