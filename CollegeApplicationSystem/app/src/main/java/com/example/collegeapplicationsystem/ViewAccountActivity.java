7cdb8bc6e6d1ac6c30a0adc1288057a594a9dca2
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2019-11-24 13:47:21 -0500

    half way done with account page start

--- CollegeApplicationSystem/app/src/main/java/com/example/collegeapplicationsystem/ViewAccountActivity.java
+++ CollegeApplicationSystem/app/src/main/java/com/example/collegeapplicationsystem/ViewAccountActivity.java
@@ -1,14 +1,84 @@
 package com.example.collegeapplicationsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ViewAccountActivity extends AppCompatActivity {
+    private static final String TAG = "USER_QUERY";
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_account);
+
+        setAccountView();
     }
+
+    private void setAccountView() {
+        setEmailView();
+        setFirstNameView();
+        setLastNameView();
+    }
+
+    private void setEmailView() {
+        TextView emailTextView = findViewById(R.id.email_textview);
+        emailTextView.setText(user.getEmail());
+    }
+
+    private void setFirstNameView() {
+        final TextView firstNameTextView = findViewById(R.id.firstname_textview);
+        db.collection("userdata").document(user.getEmail())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                firstNameTextView.setText(document.get("firstName").toString());
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void setLastNameView() {
+        final TextView lastNameTextView = findViewById(R.id.lastname_textview);
+        db.collection("userdata").document(user.getEmail())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                lastNameTextView.setText(document.get("lastName").toString());
+                            } else {
+                                Log.d(TAG, "No such document");
+                            }
+                        } else {
+                            Log.d(TAG, "get failed with ", task.getException());
+                        }
+                    }
+                });
+    }
+
 }
