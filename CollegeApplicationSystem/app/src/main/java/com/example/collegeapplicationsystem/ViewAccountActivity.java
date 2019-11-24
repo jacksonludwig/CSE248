b31daaa24b74e5cbe7f13cdc1945d49d194aac2e
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2019-11-24 17:01:16 -0500

    first name result

--- CollegeApplicationSystem/app/src/main/java/com/example/collegeapplicationsystem/ViewAccountActivity.java
+++ CollegeApplicationSystem/app/src/main/java/com/example/collegeapplicationsystem/ViewAccountActivity.java
@@ -1,11 +1,16 @@
 package com.example.collegeapplicationsystem;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -14,8 +19,9 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class ViewAccountActivity extends AppCompatActivity {
+public class ViewAccountActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     private static final String TAG = "USER_QUERY";
+    private static final int EDIT_FIRST_NAME_REQUEST_CODE = 10;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -30,8 +36,7 @@ public class ViewAccountActivity extends AppCompatActivity {
 
     private void setAccountView() {
         setEmailView();
-        setFirstNameView();
-        setLastNameView();
+        setNameAndScoreView();
     }
 
     private void setEmailView() {
@@ -39,9 +44,13 @@ public class ViewAccountActivity extends AppCompatActivity {
         emailTextView.setText(user.getEmail());
     }
 
-    private void setFirstNameView() {
+    private void setNameAndScoreView() {
         final TextView firstNameTextView = findViewById(R.id.firstname_textview);
-        db.collection("userdata").document(user.getEmail())
+        final TextView lastNameTextView = findViewById(R.id.lastname_textview);
+        final TextView mathScoreTextView = findViewById(R.id.math_textview);
+        final TextView readingScoreTextView = findViewById(R.id.reading_textview);
+        db.collection("userdata")
+                .document(user.getEmail())
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
@@ -50,27 +59,9 @@ public class ViewAccountActivity extends AppCompatActivity {
                             DocumentSnapshot document = task.getResult();
                             if (document.exists()) {
                                 firstNameTextView.setText(document.get("firstName").toString());
-                            } else {
-                                Log.d(TAG, "No such document");
-                            }
-                        } else {
-                            Log.d(TAG, "get failed with ", task.getException());
-                        }
-                    }
-                });
-    }
-
-    private void setLastNameView() {
-        final TextView lastNameTextView = findViewById(R.id.lastname_textview);
-        db.collection("userdata").document(user.getEmail())
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
                                 lastNameTextView.setText(document.get("lastName").toString());
+                                mathScoreTextView.setText(document.get("math").toString());
+                                readingScoreTextView.setText(document.get("reading").toString());
                             } else {
                                 Log.d(TAG, "No such document");
                             }
@@ -81,4 +72,44 @@ public class ViewAccountActivity extends AppCompatActivity {
                 });
     }
 
+    public void editAccount(View view) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.edit_account_menu);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.editFirst:
+                Intent intent = new Intent(getApplicationContext(), EditFirstNamePopupActivity.class);
+                startActivityForResult(intent, EDIT_FIRST_NAME_REQUEST_CODE);
+                return true;
+            case R.id.editLast:
+
+                return true;
+            case R.id.editMath:
+
+                return true;
+            case R.id.editReading:
+
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == EDIT_FIRST_NAME_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                System.out.println("NEW NAME: " + data.getStringExtra("firstName"));
+            } else {
+
+            }
+        }
+    }
 }
